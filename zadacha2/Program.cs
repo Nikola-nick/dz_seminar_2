579f7812dab1d5bec0633c959ecfd0ecdf56cfc6
nikola <124080948+Nikola-nick@users.noreply.github.com>
2023-03-04 12:23:40 +0300

    Add files via upload

---
+++ zadacha2/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine(" Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number > 999)
+{
+    number /= 10;
+}
+Console.WriteLine (number % 10);
+if (number < 100)
+{
+    Console.WriteLine("нет третьей цифры");
+}
